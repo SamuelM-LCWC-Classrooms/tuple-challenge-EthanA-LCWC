0bb83633ff30c8659b92c1a4c96f4172abef1c6f
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-09-16 13:10:46 +0000

    I made a tuple that returns the user's inputs

--- task.py
+++ task.py
@@ -1,5 +1,11 @@
 def task():
 
     # grade should be a tuple containing student name (str), subject (str) and score (int)
+    name = input("Please enter the student name: ")
+    subject = input("Please enter the student's subject: ")
+    score = int(input("Please enter the student's score out of 100 (in integer form)): "))
 
+    grade = (name, subject, score)
     return grade
+
+print(task())
